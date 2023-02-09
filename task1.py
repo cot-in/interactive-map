6c4f56172994004e0b01ac909d63c268a31f1516
ArtloK-7107549103 <118450739+ArtloK-7516405@users.noreply.github.com>
2023-02-09 17:13:39 +0400

    task1

--- task1.py
+++ task1.py
@@ -1,44 +1,41 @@
-import pygame, requests, sys, os
+import os
+import sys
+import pygame
+import requests
 
+lat = 55.703118  # широта
+lon = 37.530887  # долгота
+delta = 0.005  # зум
 
-class MapParams(object):
-    def __init__(self):
-        self.lat = 51.32060
-        self.lon = 46.813492
-        self.zoom = 7
-        self.type = "map"
+params = {
+    "ll": f"{lon},{lat}",
+    "spn": f"{delta},{delta}",
+    "l": "map",
+}
 
-    def ll(self):
-        return str(self.lon) + "," + str(self.lat)
+map_request = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_request, params=params)
 
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
 
-def load_map(mp):
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={mp.ll()}&z={mp.zoom}&l={mp.type}"
-    response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        sys.exit(1)
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
 
-    map_file = "map.png"
-    try:
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-    except IOError as ex:
-        print("Ошибка записи временного файла:", ex)
-        sys.exit(2)
-    return map_file
-
-
-
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-mp = MapParams()
-while True:
-    event = pygame.event.wait()
-    if event.type == pygame.QUIT:
-        break
-    map_file = load_map(mp)
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
-pygame.quit()
-os.remove(map_file)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+    os.remove(map_file)
+    pygame.quit()
